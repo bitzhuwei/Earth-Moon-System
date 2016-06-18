fdacb5039419c25ff0a4f33de113654505fcda51
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-18 11:26:28 +0800

    earth-moon-system-for-transform-experiment

---
+++ Assets/Planet.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour {
+
+    //private Transform transform;
+    private float currentAngle;
+
+    /// <summary>
+    /// 公转半径
+    /// </summary>
+    public float RevolutionRadius;
+
+    /// <summary>
+    /// 公转周期
+    /// </summary>
+    public float RevolutionPeriod;
+
+    // Use this for initialization
+    void Start () {
+        //this.transform = this.gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float deltaAngle = Time.deltaTime * Mathf.PI * 2 / this.RevolutionPeriod;
+        float newAngle = this.currentAngle + deltaAngle;
+        this.gameObject.transform.position = new Vector3(
+            (float)(this.RevolutionRadius * Mathf.Cos(newAngle)),
+            0,
+            (float)(this.RevolutionRadius * Mathf.Sin(newAngle)));
+
+        this.currentAngle = newAngle;
+    }
+}
---
+++ Assets/Satellite.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Satellite : MonoBehaviour
+{
+
+    private Transform planetTransform;
+    private float currentAngle;
+
+    /// <summary>
+    /// 公转半径
+    /// </summary>
+    public float RevolutionRadius;
+
+    /// <summary>
+    /// 公转周期
+    /// </summary>
+    public float RevolutionPeriod;
+    // Use this for initialization
+    void Start()
+    {
+        this.planetTransform = this.transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaAngle = Time.deltaTime * Mathf.PI * 2 / this.RevolutionPeriod;
+        float newAngle = this.currentAngle + deltaAngle;
+        this.transform.position = new Vector3(
+            (this.RevolutionRadius * Mathf.Cos(newAngle)),
+            0,
+            (this.RevolutionRadius * Mathf.Sin(newAngle)))
+            + this.planetTransform.position;
+        this.currentAngle = newAngle;
+    }
+}
